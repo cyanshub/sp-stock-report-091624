cc19639251b5dbcd3179beec45a7c539ca3b74cc
travel15740 <travel15740@gmail.com>
2024-09-16 14:37:13 +0800

    設定 middleware 應對瀏覽器 CORS 政策

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const express = require('express')
 const path = require('path')
 const methodOverride = require('method-override')
 const { apis, pages } = require('./routes')
+const cors = require('cors')
 
 // 設定應用程式
 const app = express()
@@ -18,6 +19,7 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(methodOverride('_method'))
+app.use(cors()) // 應對瀏覽器 Cross-Origin Resource Sharing 政策
 
 // 設計路由
 app.use('/api', apis)
