7c8840b7fd20359c8b600d2f60b4731ab8ff0999
travel15740 <travel15740@gmail.com>
2024-09-16 15:08:01 +0800

    加入開關: 維持遠端 Render App 的基本入站流量

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const path = require('path')
 const methodOverride = require('method-override')
 const { apis, pages } = require('./routes')
 const cors = require('cors')
+const scheduleKeepAliveReq  = require('./helpers/req-helpers')
 
 // 設定應用程式
 const app = express()
@@ -24,6 +25,9 @@ app.use(cors()) // 應對瀏覽器 Cross-Origin Resource Sharing 政策
 // 設計路由
 app.use('/api', apis)
 app.use('/', pages)
+
+// 掛載執行處理程序
+scheduleKeepAliveReq() // 定時發送請求以維持入站流量
 
 // 啟動並監聽網站
 app.listen(port, () => {
